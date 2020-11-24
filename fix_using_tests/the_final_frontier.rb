6f19f0ac09e108f882aedfde90d48bc3e2b9256e
Thomas Winter <wintert98@gmail.com>
2020-11-24 20:37:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -7,7 +7,7 @@ end
 
 def state_log(generate_star_date)
   "Captain's Log, star date #{generate_star_date}."
-  binding.pry
+  
 end
 
 def engage
