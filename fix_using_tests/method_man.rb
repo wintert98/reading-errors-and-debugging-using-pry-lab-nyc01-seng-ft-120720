187377868d8a11c7be1703968bb9f7f5866b6036
Thomas Winter <wintert98@gmail.com>
2020-11-24 20:22:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,5 +5,7 @@ def start_game(player1, player2)
 end
 
 def play_game(player2)
-  "#{player1} is better than #{player2}"
+ player = "#{player1} is better than #{player2}"
+ puts player 
+ player
 end
